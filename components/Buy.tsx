b7aff47d49b453db12d36e6d39135d4a70aec528
Cao quy Le <mikamokedo@gmail.com>
2022-11-02 14:22:05 +0700

    fix: condition

--- components/Buy.tsx
+++ components/Buy.tsx
@@ -65,7 +65,7 @@ const Buy: React.FC<BuyProps> = ({
     initialValues: {
       phone: "",
       email: "",
-      agree: true,
+      agree: false,
     },
     validationSchema: validationPhoneSchema,
     onSubmit: async (value) => {
